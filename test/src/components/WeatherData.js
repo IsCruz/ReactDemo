f6cfcaab38de2c4270d856848d69878e61a6a54a
Israel Cruz <israelcruzibarra@gmail.com>
2018-10-07 22:41:49 -0700

    change program

---
+++ test/src/components/WeatherData.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+
+
+const WeatherData = () => (
+    <div>
+    <WeatherTemperature
+      temperature={20}
+      weatherState={'cloudy'}
+    />
+    <WeatherExtraInfo humidity={80} wind={"10 m/s"}></WeatherExtraInfo>
+    </div>
+);
+
+export default WeatherData;
